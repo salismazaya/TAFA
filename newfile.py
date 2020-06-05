6c6580a49aab873b68ddd149d4d50884b9e0d4b9
Salis Mazaya Miftah Malik <50546233+salismazaya@users.noreply.github.com>
2020-06-05 12:52:50 +0700

    Delete newfile.py

--- newfile.py
+++
@@ -1,8 +0,0 @@
-import murtanto as m
-from murtanto import function as f
-
-kuki = eval(open("data.json").read())["cookies"]
-
-ses = m.Account(kuki)
-print(f.leave_group(ses,  33583936376122))
-#print(ses.session.html)
