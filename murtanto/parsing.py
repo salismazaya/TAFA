74a1371cc046547bf26c5b209be932643621396f
Salis Mazaya Miftah Malik <50546233+salismazaya@users.noreply.github.com>
2020-06-09 15:27:27 +0700

    Update parsing.py

--- murtanto/parsing.py
+++ murtanto/parsing.py
@@ -23,6 +23,18 @@ def parsing_href(html, href, one = False, bs4_class = False):
 			data = [sorting.to_mbasic(x["href"]) for x in data]
 	return data
 
+def parsing_href_regex(html, pattern, one = False, bs4_class = False):
+	data = to_bs4(html)
+	if one:
+		data = data.find("a", href = lambda x: re.search(pattern, x))
+		if not bs4_class and data != None:
+			data = sorting.to_mbasic(data["href"])
+	else:
+		data = data.find_all("a", href = lambda x: re.search(pattern, x))
+		if not bs4_class:
+			data = [sorting.to_mbasic(x["href"]) for x in data]
+	return data
+
 def getMyName(html):
 	data = to_bs4(html).find("title").text
 	return data
